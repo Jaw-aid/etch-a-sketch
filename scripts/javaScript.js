1ed0e3f0ced67c3edd43995bc5d3ad4eb60ce63f
Jaw-aid <jjali88@gmail.com>
2021-07-20 12:23:34 -0500

    Fixed createGrid function

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -6,21 +6,21 @@ let numberOfAuto = ''
 function createSquare(x) {
     let square = document.createElement('div')
     square.classList.add(`square`);
-    square.textContent = `${x}`
+    // square.textContent = `${x}`
     etchArea.appendChild(square);
 }
 
-function createGrid() {
+function createGrid(sizeOfGrid) {
     // Update gridTemplateColumns
-    for (let i = 0; i < 3; i++) {
-        numberOfAuto += 'auto'
+    for (let i = 0; i < sizeOfGrid; i++) {
+        numberOfAuto += ('auto' + ' ')
     }
     console.log(numberOfAuto)
     etchArea.style.gridTemplateColumns = numberOfAuto
     // Creates rows
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < sizeOfGrid; i++) {
         // Creates columns
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < sizeOfGrid; i++) {
             createSquare(i);
         } 
     }
