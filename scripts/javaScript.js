6030c4edbe59605bbcabf782dce66942f0187363
Jaw-aid <jjali88@gmail.com>
2021-07-20 19:01:10 -0500

    Saving WIP

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -1,7 +1,12 @@
 const etchArea = document.querySelector('.etchArea')
 let numberOfAuto = ''
 
-
+// TODO Implement repeat on grid template 
+// TODO Implement hovering to change colors
+// TODO Create text box and button 
+// TODO Make textbox and button work to change grid size
+// TODO Add in CSS changes to make website look nice 
+// TODO Add in types of etch colors
 
 function createSquare(x) {
     let square = document.createElement('div')
